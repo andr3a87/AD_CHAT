b5b908ac91f12c5fa24aa0261534f95030d743e8
andr3a87 <andrea.latella87@gmail.com>
2015-09-20 22:33:47 +0200

    Update

--- chat.js
+++ chat.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
