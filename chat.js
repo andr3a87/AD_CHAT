74b18e5884dfe4f01f0020b3f50eae533b72ddb4
andr3a87 <andrea.latella87@gmail.com>
2015-09-20 21:42:37 +0200

    Insert chat.js, update package.json

---
+++ chat.js
@@ -0,0 +1,16 @@
+/* server/chat.js
+ * Main Server File
+*/
+
+var express = require('express');
+var app = express();
+var http = require('http');
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    server();
+});
